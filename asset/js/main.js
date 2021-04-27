2904ac9992d4a5d7edf4eb40946ce01b3ee424b8
valeriomatranga <matrangav@gmail.com>
2021-04-27 12:42:51 +0200

    ingresso in lista

--- asset/js/main.js
+++ asset/js/main.js
@@ -20,7 +20,7 @@ if (parola_1.length < parola_2.length){
 }
  */
 
-var numero_1 = Number (prompt("scrivi un numero"));
+/* var numero_1 = Number (prompt("scrivi un numero"));
 var numero_2 = Number (prompt("scrivi un numero"));
 var numero_3 = Number (prompt("scrivi un numero"));
 var numero_4 = Number (prompt("scrivi un numero"));
@@ -30,6 +30,23 @@ var numero_7 = Number (prompt("scrivi un numero"));
 var numero_8 = Number (prompt("scrivi un numero"));
 var numero_9 = Number (prompt("scrivi un numero"));
 var numero_10 = Number (prompt("scrivi un numero"));
-
+ 
 var somma = numero_1 + numero_2 + numero_3 + numero_4 + numero_5 + numero_6 + numero_7 + numero_8 + numero_9 + numero_10;
 console.log(somma);
+ */
+
+var invitato = prompt('Chi sei?');
+var lista = ['vale','filippo','marco','ale'];
+var entra = false;
+
+for (var i = 0; i < lista.length; i++){
+    if (invitato == lista){
+        entra = true;
+    }
+}
+
+if (entra){
+    document.getElementById("ingresso").innerHTML = "puoi entrare";
+}else{
+    document.getElementById("ingresso").innerHTML = "Non puoi entrare";
+}
