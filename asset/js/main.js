47d58b17c16e296fdc9916730f3914b34e2353b4
valeriomatranga <matrangav@gmail.com>
2021-04-27 11:00:37 +0200

    snack2

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+var parola_1 = prompt ("scrivi una parola");
+var parola_2 = prompt ("scrivi un'altra parola");
+var grande;
+if(parola_1 > parola_2){
+    grande = parola_1;
+    console.log(grande);
+}else{
+    grande = parola_2;
+    console.log(grande);
+}
