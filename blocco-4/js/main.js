d22729337b87379e633a0ade233aaccf3b9828cf
valeriomatranga <matrangav@gmail.com>
2021-05-10 11:45:49 +0200

    modifica peso

--- blocco-4/js/main.js
+++ blocco-4/js/main.js
@@ -4,6 +4,10 @@
 //Peso = 10
 
 var palla = {
-    nome : palla,
+    nome : "palla",
     peso : 10
 }
+
+//Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+
+palla.nome = prompt('scrivi il peso della palla');
